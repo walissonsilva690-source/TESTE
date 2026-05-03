cd7f6056180a2c89b95734164d150999fd2e3878
walissonsilva690-source <walisson.silva690@gmail.com>
2026-05-03 09:53:22 -0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,47 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import Index from "./pages/Index.tsx";
+import Cenas from "./pages/Cenas.tsx";
+import Canais from "./pages/Canais.tsx";
+import Radios from "./pages/Radios.tsx";
+import ModosProntos from "./pages/ModosProntos.tsx";
+import ExecutionScreen from "./pages/ExecutionScreen";
+import NotFound from "./pages/NotFound.tsx";
+import { PreviewBar } from "@/components/ambienta/PreviewBar";
+import { ViewModeProvider } from "@/contexts/ViewModeContext";
+import { SceneStatusProvider } from "@/contexts/SceneStatusContext";
+import { AudioPlayerProvider } from "@/contexts/AudioPlayerContext";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <SceneStatusProvider>
+        <ViewModeProvider>
+          <AudioPlayerProvider>
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<Index />} />
+                <Route path="/cenas" element={<Cenas />} />
+                <Route path="/canais" element={<Canais />} />
+                <Route path="/radios" element={<Radios />} />
+                <Route path="/modos-prontos" element={<ModosProntos />} />
+                <Route path="/execution/:id" element={<ExecutionScreen />} />
+                {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </BrowserRouter>
+          </AudioPlayerProvider>
+        </ViewModeProvider>
+      </SceneStatusProvider>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
---
+++ main.tsx
@@ -0,0 +1,7 @@
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+
+const root = document.getElementById("root");
+if (root) createRoot(root).render(<App />);
+
